3cb45df07c1f72d94c33ec5cb3f785c66c99b730
limudimwork <boriskunda@gmail.com>
2018-05-23 23:16:05 +0300

    loading

--- app/src/main/java/com/happytrees/splashscreen/SplashScreen.java
+++ app/src/main/java/com/happytrees/splashscreen/SplashScreen.java
@@ -19,6 +19,8 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
 
+
+
         new Handler().postDelayed(new Runnable() {
 
             /*
